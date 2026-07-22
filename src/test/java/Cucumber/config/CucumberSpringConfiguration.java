c3e998005ab741b11888d6de5f14f37b42336d9e
MohamedAbusayath <abu240903@gmail.com>
2026-07-22 11:34:28 +0530

    Configure H2 database for CI testing

--- src/test/java/Cucumber/config/CucumberSpringConfiguration.java
+++ src/test/java/Cucumber/config/CucumberSpringConfiguration.java
@@ -4,6 +4,7 @@ import com.payment.PaymentSystemApplication;
 import com.payment.notification.PaymentEventProducer;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
         classes = PaymentSystemApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@ActiveProfiles("test")
 public class CucumberSpringConfiguration {
     @MockitoBean
     private RestTemplate restTemplate;
